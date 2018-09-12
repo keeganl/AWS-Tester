fb3150f73d6506c159c5a8996c041418d294ab74
Keegan Lawley <keeganlk.7@gmail.com>
2018-09-12 17:16:38 -0500

    commented things for testing

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-// const express = require('express')
-// const app = express()
+const express = require('express')
+const app = express()
 
 // app.use(express.static('public'))
 // app.listen(3000, () => console.log('Server running on port 3000'))
 
-var app = require('express')();
+//var app = require('express')();
 //var http = require('http').Server(app);
 var io = require('socket.io')(app);
 
 app.use(express.static('public'))
 app.listen(3000, () => console.log('Server running on port 3000'))
 
-io.on('connection', function (socket) {
-  console.log('a user connected');
-});
+// io.on('connection', function (socket) {
+//   console.log('a user connected');
+// });
 
 
