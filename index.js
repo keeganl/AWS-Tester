ab0ea97187df68c54667b14e422c08ae485346cf
Keegan Lawley <keeganlk.7@gmail.com>
2018-09-12 17:14:06 -0500

    app instead of http

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 
 var app = require('express')();
 //var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var io = require('socket.io')(app);
 
 app.use(express.static('public'))
 app.listen(3000, () => console.log('Server running on port 3000'))
