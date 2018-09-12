7b15bcc05875bca3dd083e3e1c053ee08b98b352
Keegan Lawley <keeganlk.7@gmail.com>
2018-09-12 16:39:12 -0500

    .

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ server.listen(port, () => {
 });
 
 // Routing
-app.use(express.static('public'));
+app.use(express.static('public'))
+
+app.listen(3000, () => console.log('Server running on port 3000'))
 
 // Chatroom
 
