ca09730cf63ba323e3f1285a95fa27fc26e309f0
Keegan Lawley <keeganlk.7@gmail.com>
2018-09-12 15:13:36 -0500

    Setup PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps : [{
+    name   : "app1",
+    script : "./app.js"
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-18-208-148-107.compute-1.amazonaws.com',
+      key: '~/.ssh/test_server_key.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:keeganl/AWS-Tester.git',
+      path: '/home/ubuntu/AWS-Tester',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
