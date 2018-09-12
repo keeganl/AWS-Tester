adda6b9302b9fe541526e97c76567906b0d0a5de
Keegan Lawley <keeganlk.7@gmail.com>
2018-09-12 17:01:53 -0500

    commented out server calls

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var app = express();
 var path = require('path');
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-var port = process.env.PORT || 3000;
+
 
 server.listen(port, () => {
   console.log('Server listening at port %d', port);
